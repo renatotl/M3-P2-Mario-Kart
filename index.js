fe01a5ae1d881a73d5ab5cbcd3a84b32867b62af
Renato Tenório Lino <103604513+renatotl@users.noreply.github.com>
2022-09-28 11:16:12 -0300

    fix: cors antes da rota

--- index.js
+++ index.js
@@ -6,10 +6,9 @@ const route = require('./src/routes/characters.route');
 const connectToDataBase = require('./src/database/database');
 
 connectToDataBase();
-
+app.use(cors());// o cors vem ante da rota. evita problema com o front
 app.use(express.json());
 app.use('/characters', route);
-app.use(cors());
 
 app.listen(port, () => {
   console.log(`Serve work on http://localhost:${port}`);
